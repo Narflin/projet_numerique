cb8ae527518d8ccd90d228c5cebe9fde2822e542
Narflin <70823132+Narflin@users.noreply.github.com>
2020-10-12 16:14:25 +0200

    first commit

---
+++ tests.py
@@ -0,0 +1,17 @@
+# Third-Party Libraries
+# ---------------------
+
+# Autograd & Numpy
+import autograd
+import autograd.numpy as np
+
+# Pandas
+import pandas as pd
+
+# Matplotlib
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+plt.rcParams['figure.figsize'] = [10, 10] # [width, height] (inches). 
+
+# Jupyter & IPython
+from IPython.display import display
